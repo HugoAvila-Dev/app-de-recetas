2547dada3e1a33ebe7f42ace760b2433b90db6cd
Hugo <hhugo.avila97@gmail.com>
2023-09-22 08:28:54 -0400

    feat: ✨ Funcion para eliminar elementos previos

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function iniciarApp() {
     const selectCategorias = document.querySelector('#categorias');
     selectCategorias.addEventListener('change', seleccionarCategoria);
     
-    const resultadoDiv = document.querySelector('#resultado');
+    const resultado = document.querySelector('#resultado');
 
     obtenerCategorias();
     function obtenerCategorias() {
@@ -30,6 +30,7 @@ function iniciarApp() {
 
 
     function seleccionarCategoria(e) {
+
         const categoria = e.target.value;
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
           
@@ -39,6 +40,13 @@ function iniciarApp() {
     }
 
     function mostrarRecetas(recetas = []) {
+
+        limpiarHTML(resultado);
+        
+        const heading = document.createElement('H2');
+        heading.classList.add('text-center', 'text-black', 'my-5');
+        heading.textContent = recetas.length ? 'Resultados' : 'No hay Resultados';
+        resultado.appendChild(heading);
 
         //Iterar en los resultados
         recetas.forEach(receta => {
@@ -74,8 +82,15 @@ function iniciarApp() {
             recetaCard.appendChild(recetaCardBody);
             recetaContenedor.appendChild(recetaCard);
 
-            resultadoDiv.appendChild(recetaContenedor);
+            resultado.appendChild(recetaContenedor);
+            
         })
+    }
+
+    function limpiarHTML(selector) {
+        while(selector.firstChild) {
+            selector.removeChild(selector.firstChild);
+        }
     }
 
 }
